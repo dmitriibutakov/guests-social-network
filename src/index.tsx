032ed4bf734aac2b5af8aa635dc702ff69974646
Dmitry Butakov <sportyounde@MacBook-AirDmitry.local>
2022-08-15 18:11:49 +0400

    added process.env.PUBLIC_URL and Hash router for current work gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {Provider} from "react-redux";
 import store from "./01_BLL/store";
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-        <BrowserRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
 );
 
 reportWebVitals();
