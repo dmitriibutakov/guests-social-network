6cd9371debf86310d07100798792bee76792d12c
Dmitry Butakov <sportyounde@MacBook-AirDmitry.local>
2022-08-15 18:28:35 +0400

    unknown error on gh-pages, check for debug

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {Provider} from "react-redux";
 import store from "./01_BLL/store";
-import { HashRouter } from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-        <HashRouter basename={process.env.PUBLIC_URL}>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </HashRouter>
+    <HashRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>
 );
 
 reportWebVitals();
